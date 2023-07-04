36990cf7ced07b5e7302cba558a9c6e1bd16469c
Nicholas A Christensen <nickachristensen@gmail.com>
2023-07-03 22:41:09 -0400

    Create and finish band name generator for day 1

---
+++ Day1-Band-Name-Generator/main.py
@@ -0,0 +1,8 @@
+# Greating for the program.
+print("Welcome to the band name generator")
+# Ask the user for the city they grew up in.
+city = input("Which city did you grow up in?\n")
+# Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
+# Combine the name of thei city and pet and show their band name
+print("The name of your band could be " + pet + " " + city)
