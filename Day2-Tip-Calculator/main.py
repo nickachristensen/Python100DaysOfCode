3fcf29bb6bd9d6b0a930624cfe980a22b5dbad83
Nicholas A Christensen <nickachristensen@gmail.com>
2023-07-05 19:49:19 -0400

    removed comments

--- Day2-Tip-Calculator/main.py
+++ Day2-Tip-Calculator/main.py
@@ -1,7 +1,5 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Round the result to 2 decimal places.
 print("Welcome to the tip calculator!")
+
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 15, 20, or 25? "))
 people = int(input("How many people to split the bill?"))
